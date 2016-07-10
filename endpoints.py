8954332bd58cec0da8906f628346680c1e6e312c
Fumii Chuu <gaiban@poketb.com>
2016-07-10 15:13:37 +0000

    kill repeating results + add&fix translated overrides

--- endpoints.py
+++ endpoints.py
@@ -88,8 +88,10 @@ class SuggestNames(HandlerSyncedWithMaster):
     def get(self):
         names = {value.conventional.lower(): [value.kanji, key] for key, value in starlight.data.names.items()}#conventional value.conventional
         names.update({str(key): [value.kanji, key] for key, value in starlight.data.names.items()})#chara_id
-        names.update({str(value.kanji): [value.kanji, key] for key, value in starlight.data.names.items()})#kanji
+        
+        # additional keyword set
         names.update({str(value.kana_spaced): [value.kanji, key] for key, value in starlight.data.names.items()})#kana_spaced
+        names.update({str(value.kanji): [value.kanji, key] for key, value in starlight.data.names.items()})#kanji
         names.update({str(value.translated): [value.translated + " *" , key] for key, value in starlight.data.names.items()})#translated
         names.update({str(value.translated_cht): [value.translated_cht + " *", key] for key, value in starlight.data.names.items()})#translated_cht
 		
